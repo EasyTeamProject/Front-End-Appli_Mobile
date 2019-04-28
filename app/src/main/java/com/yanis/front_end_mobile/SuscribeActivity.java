045d69e19576d61fc3aabcda142564ab52e1ce25
Yanis <yanisakli@yahoo.fr>
2019-04-28 21:54:32 +0200

    :construction: add suscribe screen

--- app/src/main/java/com/yanis/front_end_mobile/SuscribeActivity.java
+++ app/src/main/java/com/yanis/front_end_mobile/SuscribeActivity.java
@@ -5,16 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class SuscribeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_suscribe);
     }
-
-    public void onSuscribePressed(View view){
-        Intent i =new Intent(this,SuscribeActivity.class);
+    public void onBackToLoginPressed(View view){
+        Intent i =new Intent(this,LoginActivity.class);
         startActivity(i);
     }
 }

d5b74a52d7ad35927a07ac2039d8234d061e7340
Yanis <yanisakli@yahoo.fr>
2019-06-15 19:15:30 +0200

    :sparkles: remote api

--- app/src/main/java/com/yanis/front_end_mobile/LoginActivity.java
+++ app/src/main/java/com/yanis/front_end_mobile/LoginActivity.java
@@ -11,9 +11,17 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,8 +29,31 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+       /* StringBuilder result = new StringBuilder();
+        HttpURLConnection urlConnection = null;
+        try {
 
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String apiUrl = "https://api.myjson.com/bins/194g99"; // concatenate uri with base url eg: localhost:8080/ + uri
+            URL requestUrl = new URL(apiUrl);
+            urlConnection = (HttpURLConnection) requestUrl.openConnection();
+            urlConnection.connect(); // no connection is mad
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            urlConnection.disconnect();
+        }
+        Log.i("API", "onCreate: "+result.toString());
+    */
+
+    /*   RequestQueue requestQueue = Volley.newRequestQueue(this);
 
         String URL = "https://api.myjson.com/bins/194g99";
 
@@ -44,7 +75,7 @@ public class LoginActivity extends AppCompatActivity {
                 }
         );
 
-        requestQueue.add(objectRequest);
+        requestQueue.add(objectRequest);*/
     }
 
     public void onSuscribePressed(View view){

88887dc7e152218afec36c4161a242c6973072ca
Yanis <yanisakli@yahoo.fr>
2019-06-16 13:25:02 +0200

    :wrench: login API :tada: loading :fire:

--- app/src/main/java/com/yanis/front_end_mobile/LoginActivity.java
+++ app/src/main/java/com/yanis/front_end_mobile/LoginActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,36 +26,15 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
+    public EditText editTextEmailLogin;
+    public EditText editTextPasswordLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
-    /*   RequestQueue requestQueue = Volley.newRequestQueue(this);
-
-        String URL = "https://api.myjson.com/bins/194g99";
-
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                URL,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.i("API", "onResponse: " + response.toString());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.i("API", "onResponse: " + error.toString());
-                    }
-                }
-        );
-
-        requestQueue.add(objectRequest);*/
+        editTextEmailLogin=(EditText)findViewById(R.id.editTextEmailLogin);
+        editTextPasswordLogin=(EditText)findViewById(R.id.editTextPasswordLogin);
     }
 
     public void onSuscribePressed(View view){
@@ -61,8 +42,46 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void onLoginPressed(View view){
-        Intent i =new Intent(this,HomeActivity.class);
-        startActivity(i);
+
+    public void onLogin(){
+
+        PreferenceUtils.saveEmail(editTextEmailLogin.getText().toString().trim(), this);
+        PreferenceUtils.savePassword(editTextPasswordLogin.getText().toString().trim(), this);
+
+        Intent accountsIntent = new Intent(this, HomeActivity.class);
+        emptyInputEditText();
+        startActivity(accountsIntent);
+        finish();
+    }
+
+    public void login(final View view){
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        String URL = "http://192.168.1.12:3000/sessions?email="+editTextEmailLogin.getText().toString().trim()+"&password="+editTextPasswordLogin.getText().toString().trim();
+            JsonObjectRequest objectRequest = new JsonObjectRequest(
+                    Request.Method.POST,
+                    URL,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Toast.makeText(getApplicationContext(), "Login successfuly", Toast.LENGTH_SHORT).show();
+                            onLogin();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), "Error to login", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+            );
+            requestQueue.add(objectRequest);
+
+    }
+
+    private void emptyInputEditText(){
+        editTextEmailLogin.setText(null);
+        editTextPasswordLogin.setText(null);
     }
 }
---
+++ app/src/main/java/com/yanis/front_end_mobile/PreferenceUtils.java
@@ -0,0 +1,37 @@
+package com.yanis.front_end_mobile;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PreferenceUtils {
+    public PreferenceUtils(){
+
+    }
+
+    public static boolean saveEmail(String email, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString("email", email);
+        prefsEditor.apply();
+        return true;
+    }
+
+    public static String getEmail(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString("email", null);
+    }
+
+    public static boolean savePassword(String password, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor prefsEditor = prefs.edit();
+        prefsEditor.putString("password", password);
+        prefsEditor.apply();
+        return true;
+    }
+
+    public static String getPassword(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString("password", null);
+    }
+}

04fec2e8c6915e37981ab41509e02ce6acd06547
Yanis <yanisakli@yahoo.fr>
2019-06-18 01:10:51 +0200

    :lock: add a list card of event

--- app/src/main/java/com/yanis/front_end_mobile/HomeFragment.java
+++ app/src/main/java/com/yanis/front_end_mobile/HomeFragment.java
@@ -1,13 +1,31 @@
 package com.yanis.front_end_mobile;
 
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -15,6 +33,8 @@ import android.widget.TextView;
  */
 public class HomeFragment extends Fragment {
     public TextView textView;
+    public Activity context;
+    public PreferenceUtils utils;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -26,10 +46,56 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         PreferenceUtils utils = new PreferenceUtils();
+        context=getActivity();
         View v = inflater.inflate(R.layout.fragment_home, container, false);
-        textView= (TextView)v.findViewById(R.id.textViewHomeFragment);
-        textView.setText(utils.getEmail(getActivity()));
+     /*   textView= (TextView)v.findViewById(R.id.textViewHomeFragment);
+        textView.setText(utils.getEmail(context));*/
         return v;
     }
+
+    public void onStart(){
+        super.onStart();
+        getAllEvent();
+    }
+
+
+
+    private void getAllEvent() {
+
+        final String URL = "http://192.168.1.12:3000/events";
+        final String Token = utils.getToken(context);
+
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.i("info", "onResponse: OOOK ");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        Log.i("info", "onResponse: KOOO ");
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "bearer");
+                headers.put("Content-Type", "application/json");
+                headers.put("JWT",Token);
+                return headers;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(jsonObjectRequest);
+
+    }
+
 
 }

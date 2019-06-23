e913d27aa818f5696bb705d5c0dda9a75d21addb
Yanis <yanisakli@yahoo.fr>
2019-06-23 15:45:51 +0200

    :tada: Cash screen

--- app/src/main/java/com/yanis/front_end_mobile/CashEventActivity.java
+++ app/src/main/java/com/yanis/front_end_mobile/CashEventActivity.java
@@ -1,13 +1,27 @@
 package com.yanis.front_end_mobile;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CashEventActivity extends AppCompatActivity {
 
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cash_event);
+        tabLayout=(TabLayout)findViewById(R.id.tablayout_id);
+        viewPager=(ViewPager)findViewById(R.id.viewpager_id);
+
+        ViewPageAdapter viewPageAdapter=new ViewPageAdapter(getSupportFragmentManager());
+        viewPageAdapter.addFragment(new RefundFragment(),"Refund");
+        viewPageAdapter.addFragment(new ExpensesFragment(),"Expenses");
+
+        viewPager.setAdapter(viewPageAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
---
+++ app/src/main/java/com/yanis/front_end_mobile/RefundFragment.java
@@ -0,0 +1,24 @@
+package com.yanis.front_end_mobile;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RefundFragment extends Fragment {
+
+    View view;
+
+    public RefundFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.refund_fragment,container,false);
+        return view;
+    }
+}

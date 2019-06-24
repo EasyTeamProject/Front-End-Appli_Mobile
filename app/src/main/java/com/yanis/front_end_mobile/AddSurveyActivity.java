18402c10be2228f6412a61f3bd81db7445a06cda
Yanis <yanisakli@yahoo.fr>
2019-06-25 00:30:34 +0200

    :construction: survey

--- app/src/main/java/com/yanis/front_end_mobile/AddSurveyActivity.java
+++ app/src/main/java/com/yanis/front_end_mobile/AddSurveyActivity.java
@@ -5,16 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class SurveyEventActivity extends AppCompatActivity {
+public class AddSurveyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_survey_event);
+        setContentView(R.layout.activity_add_survey);
     }
 
-    public void onAddSurveyPressed(View view){
-        Intent i =new Intent(this,AddSurveyActivity.class);
-        startActivity(i);
-    }
 }

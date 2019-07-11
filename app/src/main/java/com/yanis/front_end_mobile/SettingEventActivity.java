032b0f74a7f01d86eca4ae66debb96bed8cbf5fb
Yanis <yanisakli@yahoo.fr>
2019-07-12 00:04:13 +0200

    :construction: get Event details

--- app/src/main/java/com/yanis/front_end_mobile/SettingEventActivity.java
+++ app/src/main/java/com/yanis/front_end_mobile/SettingEventActivity.java
@@ -1,13 +1,99 @@
 package com.yanis.front_end_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SettingEventActivity extends AppCompatActivity {
+
+    public EditText editTextName;
+    public EditText editTextDate;
+    public PreferenceUtils utils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_event);
+
+        editTextName= (EditText)findViewById(R.id.editTextNameEventDetail);
+        editTextDate= (EditText)findViewById(R.id.editTextDateEventDetail);
+
+        getOneEvent();
+
+
     }
+
+
+
+
+
+    private void getOneEvent() {
+
+        final String URL = "http://192.168.43.157:3000/events/5";
+        final String Token = utils.getToken(this);
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        String name = null;
+                        String Date = null;
+                        try {
+                            name = response.getString("name");
+                            Date = response.getString("date");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        editTextName.setText(name);
+                        editTextDate.setText(Date.substring(0,10));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                        Log.i("info", "onResponse: KOOO ");
+                    }
+                }) {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "bearer");
+                headers.put("Content-Type", "application/json");
+                headers.put("JWT",Token);
+                return headers;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
 }

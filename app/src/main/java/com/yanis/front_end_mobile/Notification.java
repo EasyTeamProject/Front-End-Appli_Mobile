e6102997541ef473cbd0bcd09d2b2853ad5378d2
Yanis <yanisakli@yahoo.fr>
2019-07-18 23:03:14 +0200

    :construction: list of notification

---
+++ app/src/main/java/com/yanis/front_end_mobile/Notification.java
@@ -0,0 +1,27 @@
+package com.yanis.front_end_mobile;
+
+public class Notification {
+    String event_name;
+    String user_id;
+
+    public Notification(String event_name, String user_id) {
+        this.event_name = event_name;
+        this.user_id = user_id;
+    }
+
+    public String getEvent_name() {
+        return event_name;
+    }
+
+    public void setEvent_name(String event_name) {
+        this.event_name = event_name;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
+    }
+}
